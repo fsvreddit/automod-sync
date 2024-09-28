30ff4b3e4a3890b75da62421d3add5cecc2db8c7
fsvreddit <97185438+fsvreddit@users.noreply.github.com>
2024-09-28 10:24:44 +0100

    Add additional test

--- src/utility.test.ts
+++ src/utility.test.ts
@@ -1,4 +1,5 @@
-import {replaceSpecialCharacters} from "./utility.js";
+import { replacedRuleWithActionsPreserved } from "./automoderator.js";
+import { replaceSpecialCharacters } from "./utility.js";
 
 test("Replacement of special characters", () => {
     const input = "# 🏴 is a flag";
@@ -6,4 +7,10 @@ test("Replacement of special characters", () => {
 
     const actual = replaceSpecialCharacters(input);
     expect(actual).toEqual(expected);
+});
+
+test("Replacement of unicode characters lower ranges", () => {
+    const input = "body (regex): [ \"\\U00000400-\\U000004FF+\" ]";
+    const output = replacedRuleWithActionsPreserved(input, input);
+    expect(output).toEqual(input);
 });
